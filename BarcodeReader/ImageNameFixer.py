7434508d6d985507b519e8d77183db3685f46cd6
ccampell <ccampell.asu@gmail.com>
2019-01-29 21:29:44 -0500

    Added code to resize image while preserving aspect ratio.

--- BarcodeReader/ImageNameFixer.py
+++ BarcodeReader/ImageNameFixer.py
@@ -11,6 +11,8 @@ import shutil
 from PIL import Image
 from pyzbar import pyzbar
 from pyzbar.pyzbar import ZBarSymbol
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Valid image types that this program will attempt to read barcodes from
 # NOTE: Extensions are case insensitive, if 'jpg' is in this list then both: 'MyFile.jpg' and 'MyFile.JPG' will be valid
@@ -44,6 +46,26 @@ def ensure_directory_is_readable_and_writeable(dir):
         return False
 
 
+def resize_image(img, new_width):
+    """
+    resize_image: Resizes the provided image to the specified width while preserving aspect ratio. This appears to be
+    what the pearl version of the script does via ImageMagick geometry argument.
+    :url :
+    :param new_width:
+    :return:
+    """
+    img_clone = img.copy()
+    maximum_width, maximum_height = img.size
+    resize_dims = new_width, maximum_height
+    try:
+        img_clone.thumbnail(resize_dims, Image.ANTIALIAS)
+        # img_clone.thumbnail(resize_dims)
+    except Exception as err:
+        print('Could not create thumbnail for image: %s. Stack trace: %s' % (img, err))
+        return None
+    return img_clone
+
+
 def main():
     # Create the subdirectories used by this script if they don't already exist:
     if not os.path.exists(source_img_dir):
@@ -72,7 +94,7 @@ def main():
         for i, fname in enumerate(file_list):
             # Ensure that the image extension is in the list of programmer specified valid extension file types:
             if os.path.basename(fname).lower().split('.')[1] in VALID_EXTENSIONS:
-                print('\t[%d/%d] Running Optical Character Recognition (OCR) on image \'%s\':' % (i, num_files, fname))
+                print('\t[%d/%d] Running Optical Character Recognition (OCR) on image \'%s\':' % (i+1, num_files, fname))
                 # Attempt to open the image using PIL:
                 try:
                     img = Image.open(os.path.join(dir_name, fname))
@@ -101,11 +123,42 @@ def main():
                     shutil.copy(src=os.path.join(dir_name, fname), dst=os.path.join(renamed_img_dir, new_img_name + source_img_ext))
                 else:
                     # OCR failed
-                    print('\t\tFailed to decode barcode.')
-                    ocr_failure_images.append(os.path.join(dir_name, fname))
-                    num_ocr_fails += 1
-                    # Copy image to failed_image_dir:
-                    shutil.copy(os.path.join(dir_name, fname), os.path.join(failed_img_dir, fname))
+                    print('\t\tFailed to decode barcode. Attempting to resize and OCR...')
+                    # plt.imshow(np.array(img))
+                    # plt.show()
+                    # plt.clf()
+                    # See: http://www.imagemagick.org/script/command-line-processing.php#geometry for magic number 3000x
+                    resized_img = resize_image(img, new_width=3000)
+                    # plt.imshow(np.array(resized_img))
+                    # plt.show()
+                    print('\t\t\tImage down-sampled to new aspect ratio: %s. Attempting to OCR...' % (resized_img.size,))
+                    decoded_img = pyzbar.decode(resized_img)
+                    if decoded_img:
+                        # OCR Success:
+                        ocr_success_images.append(os.path.join(dir_name, fname))
+                        num_ocr_success += 1
+                        print('\t\t\t%s' % decoded_img)
+                        # Get the new image name from the barcode:
+                        new_img_name = decoded_img[0][0].decode()
+                        # Get the old image extension:
+                        source_img_ext = os.path.splitext(img.filename)[1]
+                        # Copy and rename the image:
+                        shutil.copy(src=os.path.join(dir_name, fname), dst=os.path.join(renamed_img_dir, new_img_name + source_img_ext))
+                    else:
+                        # OCR Failure
+                        print('\t\t\tOCR failed on the resized image. Copying image: \'%s\' to the failed image '
+                              'directory: \'%s\'.' % (fname, failed_img_dir))
+                        ocr_failure_images.append(os.path.join(dir_name, fname))
+                        num_ocr_fails += 1
+                        # Copy image to failed_image_dir:
+                        shutil.copy(os.path.join(dir_name, fname), os.path.join(failed_img_dir, fname))
+
+        # Dump list of failed OCR images to root directory:
+        with open('failed_images.txt', 'w') as fp:
+            for image_name in ocr_failure_images:
+                fp.write(os.path.basename(image_name) + '\n')
+        print('Script finished. Failed to OCR the images listed in the \'failed_images.txt\' file. Exiting...')
+        exit(0)
 
 
 if __name__ == '__main__':

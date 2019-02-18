b97d1c0dd3e711d802fde8da3bc50d50de454b6b
ccampell <ccampell.asu@gmail.com>
2019-02-17 20:07:15 -0500

    Modified code to allow the user to enter directories not relative to the install location. This is so that the user doesn't need to copy very large image directories to the script install location.

--- BarcodeReader/ImageNameFixer.py
+++ BarcodeReader/ImageNameFixer.py
@@ -4,6 +4,7 @@ ImageNameFixer.py
 
 __author__ = 'Chris Campell'
 __created__ = '1/14/2019'
+__updated__ = '2/17/2019'
 
 import os
 import sys
@@ -19,7 +20,7 @@ import numpy as np
 VALID_EXTENSIONS = ['jpg', 'jpeg', 'png']
 
 
-def ensure_directory_is_readable_and_writeable(dir):
+def _directory_exists_and_is_readable_and_writeable(dir):
     """
     ensure_directory_is_readable_and_writeable: Ensures that the directory:
         1) Exists
@@ -34,6 +35,7 @@ def ensure_directory_is_readable_and_writeable(dir):
         if os.access(dir, os.R_OK):
             # Ensure the directory has write access:
             if os.access(dir, os.W_OK):
+                print('INFO: Ensured directory: \'%s\' exists, is readable, and is writeable.' % dir)
                 return True
             else:
                 print('Error: This script does not have OS permission to write to the provided directory: \'%s\'' % dir)
@@ -49,7 +51,7 @@ def ensure_directory_is_readable_and_writeable(dir):
 def resize_image(img, new_width):
     """
     resize_image: Resizes the provided image to the specified width while preserving aspect ratio. This appears to be
-    what the pearl version of the script does via ImageMagick geometry argument.
+    what the pearl version of the script does via ImageMagick's geometry argument.
     :url :
     :param new_width:
     :return:
@@ -66,38 +68,183 @@ def resize_image(img, new_width):
     return img_clone
 
 
-def main():
-    # Create the subdirectories used by this script if they don't already exist:
-    if not os.path.exists(source_img_dir):
-        os.mkdir(source_img_dir)
-    if not os.path.exists(renamed_img_dir):
-        os.mkdir(renamed_img_dir)
-    if not os.path.exists(failed_img_dir):
-        os.mkdir(failed_img_dir)
+# def main():
+#     # Create the subdirectories used by this script if they don't already exist:
+#     if not os.path.exists(source_img_dir):
+#         os.mkdir(source_img_dir)
+#     if not os.path.exists(renamed_img_dir):
+#         os.mkdir(renamed_img_dir)
+#     if not os.path.exists(failed_img_dir):
+#         os.mkdir(failed_img_dir)
+#
+#     # Traverse the source image directory. The topmost folder is the parent or 'root' folder:
+#     for dir_name, sub_dir_list, file_list in os.walk(source_img_dir):
+#         # Skip the parent directory:
+#         if dir_name == root_dir:
+#             continue
+#
+#         print('Walking directory : %s' % dir_name)
+#         num_files = len(file_list)
+#         openable_images = []
+#         unopenable_images = []
+#         ocr_success_images = []
+#         ocr_failure_images = []
+#         num_ocr_success = 0
+#         num_ocr_fails = 0
+#         num_openable_files = 0
+#
+#         for i, fname in enumerate(file_list):
+#             # Ensure that the image extension is in the list of programmer specified valid extension file types:
+#             if os.path.basename(fname).lower().split('.')[1] in VALID_EXTENSIONS:
+#                 print('\t[%d/%d] Running Optical Character Recognition (OCR) on image \'%s\':' % (i+1, num_files, fname))
+#                 # Attempt to open the image using PIL:
+#                 try:
+#                     img = Image.open(os.path.join(dir_name, fname))
+#                     # img = Image.open(os.path.join(dir_name, fname)).convert('RGB')
+#                     openable_images.append(os.path.join(dir_name, fname))
+#                     num_openable_files += 1
+#                     if os.path.getsize(os.path.join(dir_name, fname)) == 0:
+#                         print('\t\tWARNING: Image \'%s\' is of size 0 bytes.' % fname)
+#                 except Exception as err:
+#                     print('\t\tERROR: Failed to open image \'%s\'. Proceeding without this image.' % fname)
+#                     unopenable_images.append(os.path.join(dir_name, fname))
+#                     # Copy image to failed_image_dir:
+#                     shutil.copy(os.path.join(dir_name, fname), os.path.join(failed_img_dir, fname))
+#                     continue
+#                 # The image has been opened successfully, now try running OCR on the barcode:
+#                 decoded_img = pyzbar.decode(img)
+#                 if decoded_img:
+#                     # OCR worked
+#                     ocr_success_images.append(os.path.join(dir_name, fname))
+#                     num_ocr_success += 1
+#                     print('\t\t%s' % decoded_img)
+#                     # Get the new image name from the barcode:
+#                     new_img_name = decoded_img[0][0].decode()
+#                     # Get the old image extension:
+#                     source_img_ext = os.path.splitext(img.filename)[1]
+#                     # Copy and rename the image:
+#                     shutil.copy(src=os.path.join(dir_name, fname), dst=os.path.join(renamed_img_dir, new_img_name + source_img_ext))
+#                 else:
+#                     # OCR failed
+#                     print('\t\tFailed to decode barcode. Attempting to OCR with RGB Image')
+#                     decoded_img = pyzbar.decode(img.convert('RGB'))
+#                     if decoded_img:
+#                         # OCR Success:
+#                         print('\t\t\t%s' % decoded_img)
+#                     else:
+#                         # OCR Failure:
+#                         print('\t\t\tOCR failed on the RGB image.')
+#
+#                     # print('\t\tFailed to decode barcode. Attempting to resize and OCR...')
+#                     # plt.imshow(np.array(img))
+#                     # plt.show()
+#                     # plt.clf()
+#                     # See: http://www.imagemagick.org/script/command-line-processing.php#geometry for magic number 3000x
+#                     # resized_img = resize_image(img, new_width=3000)
+#                     # plt.imshow(np.array(resized_img))
+#                     # plt.show()
+#                     # print('\t\t\tImage down-sampled to new aspect ratio: %s. Attempting to OCR...' % (resized_img.size,))
+#                     # decoded_img = pyzbar.decode(resized_img)
+#                     # if decoded_img:
+#                     #     # OCR Success:
+#                     #     ocr_success_images.append(os.path.join(dir_name, fname))
+#                     #     num_ocr_success += 1
+#                     #     print('\t\t\t%s' % decoded_img)
+#                     #     # Get the new image name from the barcode:
+#                     #     new_img_name = decoded_img[0][0].decode()
+#                     #     # Get the old image extension:
+#                     #     source_img_ext = os.path.splitext(img.filename)[1]
+#                     #     # Copy and rename the image:
+#                     #     shutil.copy(src=os.path.join(dir_name, fname), dst=os.path.join(renamed_img_dir, new_img_name + source_img_ext))
+#                     # else:
+#                     #     # OCR Failure
+#                     #     print('\t\t\tOCR failed on the resized image. Copying image: \'%s\' to the failed image '
+#                     #           'directory: \'%s\'.' % (fname, failed_img_dir))
+#                     #     ocr_failure_images.append(os.path.join(dir_name, fname))
+#                     #     num_ocr_fails += 1
+#                     #     # Copy image to failed_image_dir:
+#                     #     shutil.copy(os.path.join(dir_name, fname), os.path.join(failed_img_dir, fname))
+#
+#         # Dump list of failed OCR images to root directory:
+#         with open('failed_images.txt', 'w') as fp:
+#             for image_name in ocr_failure_images:
+#                 fp.write(os.path.basename(image_name) + '\n')
+#         print('Script finished. Failed to OCR the images listed in the \'failed_images.txt\' file. Exiting...')
+#         exit(0)
 
+
+def _get_mislabeled_image_dir_from_user():
+    while True:
+        mislabeled_img_dir = input('Enter the file path to the folder containing mislabeled images here (you can always '
+                                   'copy and paste it): ')
+        if _directory_exists_and_is_readable_and_writeable(mislabeled_img_dir):
+            break
+        else:
+            print('Try again.')
+    return mislabeled_img_dir
+
+
+def _get_failed_image_dir_from_user():
+    while True:
+        failed_img_dir = input('Enter the file path to the folder where you wish mislabeled images (which could not be '
+                               'automatically re-named) to be copied to: ')
+        if _directory_exists_and_is_readable_and_writeable(failed_img_dir):
+            break
+        else:
+            print('Try again.')
+    return failed_img_dir
+
+
+def _get_renamed_image_dir_from_user():
+    while True:
+        renamed_img_dir = input('Enter the file path to the folder where you wish automatically renamed images to be '
+                                'copied to: ')
+        if _directory_exists_and_is_readable_and_writeable(renamed_img_dir):
+            break
+        else:
+            print('Try again.')
+    return renamed_img_dir
+
+
+def _get_wants_failed_images_copied_from_user():
+    wants_failed_images_copied = None
+    while wants_failed_images_copied is None:
+        wants_failed_images_copied_verbatim = input('Do you wish for mislabeled images that are not able to be '
+                                                    'automatically re-named to be copied over to a separate folder? '
+                                                    'Enter {(y)es, (n)o}: ')
+        if wants_failed_images_copied_verbatim.lower() == 'yes' or wants_failed_images_copied_verbatim.lower() == 'y':
+            wants_failed_images_copied = True
+        elif wants_failed_images_copied_verbatim.lower() == 'no' or wants_failed_images_copied_verbatim.lower() == 'n':
+            wants_failed_images_copied = False
+        else:
+            print('Failed to interpret user entered response: \'%s\'. Please enter either \'yes\' (\'y\') or \'no\' '
+                  '(\'n\'). Try again!' % wants_failed_images_copied_verbatim)
+    return wants_failed_images_copied
+
+
+def _ocr_and_rename_all_images_in_dir(mislabeled_img_dir, renamed_img_dir, failed_img_dir=None):
     # Traverse the source image directory. The topmost folder is the parent or 'root' folder:
-    for dir_name, sub_dir_list, file_list in os.walk(source_img_dir):
-        # Skip the parent directory:
-        if dir_name == root_dir:
-            continue
+    openable_images = []
+    unopenable_images = []
+    ocr_success_images = []
+    ocr_failure_images = []
+    # num_ocr_successes = 0
+    # num_ocr_failures = 0
+    num_openable_files = 0
 
-        print('Walking directory : %s' % dir_name)
-        num_files = len(file_list)
-        openable_images = []
-        unopenable_images = []
-        ocr_success_images = []
-        ocr_failure_images = []
-        num_ocr_success = 0
-        num_ocr_fails = 0
-        num_openable_files = 0
-
+    for dir_name, sub_dir_list, file_list in os.walk(mislabeled_img_dir):
+        print('INFO: Walking directory: \'%s\'' % dir_name)
+        num_files_in_dir = len(file_list)
+        num_ocr_failures_in_dir = 0
+        num_ocr_successes_in_dir = 0
         for i, fname in enumerate(file_list):
             # Ensure that the image extension is in the list of programmer specified valid extension file types:
             if os.path.basename(fname).lower().split('.')[1] in VALID_EXTENSIONS:
-                print('\t[%d/%d] Running Optical Character Recognition (OCR) on image \'%s\':' % (i+1, num_files, fname))
+                print('\t[%d/%d] Running Optical Character Recognition (OCR) on image \'%s\':' % (i+1, num_files_in_dir, fname))
                 # Attempt to open the image using PIL:
                 try:
                     img = Image.open(os.path.join(dir_name, fname))
+                    # img = Image.open(os.path.join(dir_name, fname)).convert('RGB')
                     openable_images.append(os.path.join(dir_name, fname))
                     num_openable_files += 1
                     if os.path.getsize(os.path.join(dir_name, fname)) == 0:
@@ -105,15 +252,16 @@ def main():
                 except Exception as err:
                     print('\t\tERROR: Failed to open image \'%s\'. Proceeding without this image.' % fname)
                     unopenable_images.append(os.path.join(dir_name, fname))
-                    # Copy image to failed_image_dir:
-                    shutil.copy(os.path.join(dir_name, fname), os.path.join(failed_img_dir, fname))
+                    if failed_img_dir is not None:
+                        # Copy image to failed_image_dir:
+                        shutil.copy(os.path.join(dir_name, fname), os.path.join(failed_img_dir, fname))
                     continue
                 # The image has been opened successfully, now try running OCR on the barcode:
                 decoded_img = pyzbar.decode(img)
                 if decoded_img:
                     # OCR worked
                     ocr_success_images.append(os.path.join(dir_name, fname))
-                    num_ocr_success += 1
+                    num_ocr_successes_in_dir += 1
                     print('\t\t%s' % decoded_img)
                     # Get the new image name from the barcode:
                     new_img_name = decoded_img[0][0].decode()
@@ -123,60 +271,56 @@ def main():
                     shutil.copy(src=os.path.join(dir_name, fname), dst=os.path.join(renamed_img_dir, new_img_name + source_img_ext))
                 else:
                     # OCR failed
-                    print('\t\tFailed to decode barcode. Attempting to resize and OCR...')
-                    # plt.imshow(np.array(img))
-                    # plt.show()
-                    # plt.clf()
-                    # See: http://www.imagemagick.org/script/command-line-processing.php#geometry for magic number 3000x
-                    resized_img = resize_image(img, new_width=3000)
-                    # plt.imshow(np.array(resized_img))
-                    # plt.show()
-                    print('\t\t\tImage down-sampled to new aspect ratio: %s. Attempting to OCR...' % (resized_img.size,))
-                    decoded_img = pyzbar.decode(resized_img)
-                    if decoded_img:
-                        # OCR Success:
-                        ocr_success_images.append(os.path.join(dir_name, fname))
-                        num_ocr_success += 1
-                        print('\t\t\t%s' % decoded_img)
-                        # Get the new image name from the barcode:
-                        new_img_name = decoded_img[0][0].decode()
-                        # Get the old image extension:
-                        source_img_ext = os.path.splitext(img.filename)[1]
-                        # Copy and rename the image:
-                        shutil.copy(src=os.path.join(dir_name, fname), dst=os.path.join(renamed_img_dir, new_img_name + source_img_ext))
-                    else:
-                        # OCR Failure
-                        print('\t\t\tOCR failed on the resized image. Copying image: \'%s\' to the failed image '
-                              'directory: \'%s\'.' % (fname, failed_img_dir))
-                        ocr_failure_images.append(os.path.join(dir_name, fname))
-                        num_ocr_fails += 1
-                        # Copy image to failed_image_dir:
-                        shutil.copy(os.path.join(dir_name, fname), os.path.join(failed_img_dir, fname))
+                    print('\t\tFailed to OCR and decode barcode.')
+                    num_ocr_failures_in_dir += 1
+                    ocr_failure_images.append(os.path.join(dir_name, fname))
+        ocr_success_rate_in_dir = ((num_ocr_successes_in_dir * 100)/num_files_in_dir)
+        print('INFO: Directory OCR Stats: \n\tOCR Success Rate: %.2f%%\n\tNumber of renamed images: %d\n\tNumber of remaining mislabeled images: %d' % (ocr_success_rate_in_dir, num_ocr_successes_in_dir, num_ocr_failures_in_dir))
+    print('INFO: No more sub-directories identified. Global OCR and renaming finished. Dumping list of failed OCR images to: \'%s\'' % '/BarcodeReader/failed_images.txt')
+    with open('failed_images.txt', 'w') as fp:
+        for image_name in ocr_failure_images:
+            fp.write(os.path.basename(image_name) + '\n')
+    print('INFO: Script finished. Failed to OCR the images listed in the \'failed_images.txt\' file. Exiting...')
+    exit(0)
 
-        # Dump list of failed OCR images to root directory:
-        with open('failed_images.txt', 'w') as fp:
-            for image_name in ocr_failure_images:
-                fp.write(os.path.basename(image_name) + '\n')
-        print('Script finished. Failed to OCR the images listed in the \'failed_images.txt\' file. Exiting...')
-        exit(0)
+
+def main():
+    # Recall that directories are by default relative to the invoked script's location:
+    root_dir = '../BarcodeReader'
+    mislabeled_img_dir = _get_mislabeled_image_dir_from_user()
+    renamed_img_dir = _get_renamed_image_dir_from_user()
+    user_wants_failed_images_copied = _get_wants_failed_images_copied_from_user()
+    if user_wants_failed_images_copied:
+        failed_img_dir = _get_failed_image_dir_from_user()
+    else:
+        failed_img_dir = None
+    _ocr_and_rename_all_images_in_dir(
+        mislabeled_img_dir=mislabeled_img_dir,
+        renamed_img_dir=renamed_img_dir,
+        failed_img_dir=failed_img_dir
+    )
+
 
 
 if __name__ == '__main__':
-    # Directories are relative to this script's location:
-    root_dir = '../BarcodeReader'
-    source_img_dir = 'MislabeledImages'
-    renamed_img_dir = 'RenamedImages'
-    failed_img_dir = 'FailedImages'
-
-    # Ensure that the user installed the script in a directory with read and write permissions:
-    if ensure_directory_is_readable_and_writeable(root_dir):
-        print('INFO: Ensured script install directory exists, is readable, and is writeable.')
-        if not os.path.isdir(source_img_dir):
-            os.mkdir(source_img_dir)
-            print('INFO: Created directory \'%s\' for you. Please place all mislabeled images in this folder, '
-                  'and run the script again.' % source_img_dir)
-            exit(0)
-        main()
-    else:
-        print('Fatal Error: Terminating script...')
-        sys.exit(-1)
+    main()
+    # # Directories are relative to this script's location:
+    # root_dir = '../BarcodeReader'
+    # # source_img_dir = 'MislabeledImages'
+    # renamed_img_dir = 'RenamedImages'
+    # failed_img_dir = 'FailedImages'
+    #
+    #
+    #
+    # # Ensure that the user installed the script in a directory with read and write permissions:
+    # if ensure_directory_is_readable_and_writeable(root_dir):
+    #     print('INFO: Ensured script install directory exists, is readable, and is writeable.')
+    #     if not os.path.isdir(source_img_dir):
+    #         os.mkdir(source_img_dir)
+    #         print('INFO: Created directory \'%s\' for you. Please place all mislabeled images in this folder, '
+    #               'and run the script again.' % source_img_dir)
+    #         exit(0)
+    #     main()
+    # else:
+    #     print('Fatal Error: Terminating script...')
+    #     sys.exit(-1)
